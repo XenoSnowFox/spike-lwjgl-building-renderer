a6e342a6fd884c29969a2c8ce834f69b047a0d95
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-07-10 17:25:49 +1000

    util method for loading a file one line at a time

--- src/main/java/spike/lwjglbuildingrenderer/Utils.java
+++ src/main/java/spike/lwjglbuildingrenderer/Utils.java
@@ -1,8 +1,13 @@
 package spike.lwjglbuildingrenderer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Utils {
 
@@ -22,6 +27,27 @@ public class Utils {
 		} catch (IOException ex) {
 			throw new RuntimeException("Unable to load properties from `" + resourceName + "`", ex);
 		}
+	}
+
+
+	public static String loadResource(String fileName) throws Exception {
+		String result;
+		try (InputStream in = Utils.class.getResourceAsStream(fileName);
+				Scanner scanner = new Scanner(in, java.nio.charset.StandardCharsets.UTF_8.name())) {
+			result = scanner.useDelimiter("\\A").next();
+		}
+		return result;
+	}
+
+	public static List<String> readAllLines(String fileName) throws Exception {
+		List<String> list = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(Class.forName(Utils.class.getName()).getResourceAsStream(fileName)))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				list.add(line);
+			}
+		}
+		return list;
 	}
 
 	/**
